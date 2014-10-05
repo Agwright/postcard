e7cc0808e7f76a68faebd0018f7b84e674026a44
Antony Wright <antonywright@Antonys-iMac-4.local>
2014-10-05 18:56:45 +0100

    First Commit

--- postcard/ViewController.swift
+++ postcard/ViewController.swift
@@ -10,6 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var massageLabel: UILabel!
+    @IBOutlet weak var enterNameTextField: UITextField!
+    @IBOutlet weak var enterMessageTextField: UITextField!
+    @IBOutlet weak var sentEmailButton: UIButton!
+    @IBOutlet var theView: UIView!
+    
+    
+    @IBAction func sendMailButtonPressed(sender: UIButton) {
+        
+        var textEntered = enterMessageTextField.text;
+        massageLabel.text = textEntered
+        
+        massageLabel.hidden = false
+        massageLabel.backgroundColor = UIColor.lightGrayColor()
+        massageLabel.textColor = UIColor.whiteColor()
+        enterMessageTextField.resignFirstResponder()
+        enterNameTextField.resignFirstResponder()
+        
+        enterMessageTextField.text = ""
+        enterNameTextField.text = ""
+        
+        sentEmailButton.setTitle("Mail Send", forState: UIControlState.Normal);
+        theView.backgroundColor = UIColor.lightGrayColor()  
+        
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
